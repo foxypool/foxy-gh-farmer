7843820203b8c2e819abda2837d2a87b60fd46f4
Felix Brucker <felix@felixbrucker.com>
2023-10-31 12:58:01 +0530

    Improve `join-pool` command resilience

--- foxy_gh_farmer/cmds/join_pool.py
+++ foxy_gh_farmer/cmds/join_pool.py
@@ -1,13 +1,13 @@
 import asyncio
 import functools
+import time
 from asyncio import sleep
 from datetime import datetime
 from pathlib import Path
-from typing import Dict, Any, List, Tuple
+from typing import Dict, Any, List, Tuple, Callable, Awaitable
 
 import click
 from chia.cmds.cmds_util import get_wallet
-from chia.cmds.plotnft_funcs import submit_tx_with_confirmation
 from chia.daemon.client import connect_to_daemon_and_validate, DaemonProxy
 from chia.rpc.wallet_rpc_client import WalletRpcClient
 from chia.server.outbound_message import NodeType
@@ -15,6 +15,7 @@ from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.byte_types import hexstr_to_bytes
 from chia.util.config import load_config
 from chia.util.ints import uint64, uint16
+from chia.wallet.transaction_record import TransactionRecord
 from chia.wallet.util.wallet_types import WalletType
 from humanize import naturaldelta
 from yaspin import yaspin
@@ -68,7 +69,8 @@ async def join_pool(
             while not await is_wallet_reachable():
                 await sleep(3)
 
-        fingerprint = await get_wallet(foxy_root, wallet_rpc, fingerprint=None)
+        # Select wallet to sync
+        await get_wallet(foxy_root, wallet_rpc, fingerprint=None)
 
         await wait_for_wallet_sync(wallet_rpc)
 
@@ -79,7 +81,7 @@ async def join_pool(
 
             return
 
-        await join_plot_nfts_to_pool(wallet_rpc, plot_nfts_not_pooling_with_foxy, fingerprint)
+        await join_plot_nfts_to_pool(wallet_rpc, plot_nfts_not_pooling_with_foxy)
 
         await wait_for_wallet_sync(wallet_rpc)
 
@@ -109,7 +111,7 @@ async def stop_wallet(daemon_proxy: DaemonProxy, close_daemon: bool):
             print(f"Stop daemon failed {r}")
 
 
-async def join_plot_nfts_to_pool(wallet_client: WalletRpcClient, plot_nfts: List[Dict[str, Any]], fingerprint: int):
+async def join_plot_nfts_to_pool(wallet_client: WalletRpcClient, plot_nfts: List[Dict[str, Any]]):
     plot_nfts_by_launcher_id: Dict[bytes32, Dict[str, Any]] = {
         bytes32.from_hexstr(plot_nft["launcher_id"]): plot_nft
         for plot_nft in plot_nfts
@@ -125,16 +127,24 @@ async def join_plot_nfts_to_pool(wallet_client: WalletRpcClient, plot_nfts: List
         wallet_id = pool_wallet["id"]
         pool_wallet_info, _ = await wallet_client.pw_status(wallet_id)
         plot_nft = plot_nfts_by_launcher_id.get(pool_wallet_info.launcher_id)
+        launcher_id = pool_wallet_info.launcher_id.hex()
         if plot_nft is None:
-            print(f"Could not find PlotNFT for LauncherID {pool_wallet_info.launcher_id.hex()} in config.yaml, skipping")
+            print(f"❌ Could not find PlotNFT with LauncherID {launcher_id} in config.yaml, skipping")
+
             continue
-        with yaspin(text=f"Joining PlotNFT with LauncherID {pool_wallet_info.launcher_id.hex()} to the pool ..."):
+        with yaspin(text=f"Joining PlotNFT with LauncherID {launcher_id} to the pool ...") as spinner:
             while not (await wallet_client.get_synced()):
                 await sleep(5)
-            await join_plot_nft_to_pool(wallet_client, pool_info, wallet_id, fingerprint)
+            try:
+                await join_plot_nft_to_pool(wallet_client, pool_info, wallet_id)
+                spinner.stop()
+                print(f"✅ Submitted the pool join transaction for PlotNFT with LauncherID {launcher_id}")
+            except Exception as e:
+                spinner.stop()
+                print(f"❌ Could not join PlotNFT with LauncherID {launcher_id} because an error occurred: {e}")
 
 
-async def join_plot_nft_to_pool(wallet_client: WalletRpcClient, pool_info: Dict[str, Any], wallet_id: int, fingerprint: int):
+async def join_plot_nft_to_pool(wallet_client: WalletRpcClient, pool_info: Dict[str, Any], wallet_id: int):
     func = functools.partial(
         wallet_client.pw_join_pool,
         wallet_id,
@@ -143,7 +153,7 @@ async def join_plot_nft_to_pool(wallet_client: WalletRpcClient, pool_info: Dict[
         pool_info["relative_lock_height"],
         uint64(0),
     )
-    await submit_tx_with_confirmation("", False, func, wallet_client, fingerprint, wallet_id)
+    await submit_tx_with_confirmation(func, wallet_client)
     await sleep(15)
 
 
@@ -153,11 +163,12 @@ async def wait_for_wallet_sync(wallet_client: WalletRpcClient):
             connected_full_nodes_count = len(await wallet_client.get_connections(node_type=NodeType.FULL_NODE))
             wallet_height = await wallet_client.get_height_info()
             relative_time = "N/A"
-            try:
-                wallet_timestamp = await wallet_client.get_timestamp_for_height(wallet_height)
-                relative_time = naturaldelta(datetime.now() - datetime.fromtimestamp(float(wallet_timestamp)))
-            except:
-                pass
+            if connected_full_nodes_count > 0:
+                try:
+                    wallet_timestamp = await wallet_client.get_timestamp_for_height(wallet_height)
+                    relative_time = naturaldelta(datetime.now() - datetime.fromtimestamp(float(wallet_timestamp)))
+                except:
+                    pass
             spinner.text = f"Waiting for the wallet to sync (peers={connected_full_nodes_count}, height={wallet_height}, {relative_time} behind) ..."
 
         while len(await wallet_client.get_connections(node_type=NodeType.FULL_NODE)) < 2:
@@ -193,3 +204,17 @@ def get_plot_nft_not_pooling_with_foxy(config: Dict[str, Any]) -> List[Dict[str,
         return []
 
     return list(filter(lambda pool: "foxypool.io" not in pool["pool_url"], config["pool"]["pool_list"]))
+
+
+async def submit_tx_with_confirmation(
+    func: Callable[[], Awaitable[Dict[str, Any]]],
+    wallet_client: WalletRpcClient,
+) -> None:
+    result = await func()
+    tx_record: TransactionRecord = result["transaction"]
+    start = time.time()
+    while time.time() - start < 15:
+        await asyncio.sleep(0.1)
+        tx = await wallet_client.get_transaction(1, tx_record.name)
+        if len(tx.sent_to) > 0:
+            return None
