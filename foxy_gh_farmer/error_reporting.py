62abfb250aaa476e3cfb276b4ce1e93a3e3d4c68
Felix Brucker <felix@felixbrucker.com>
2023-11-01 22:35:33 +0530

    Do not report error logs

--- foxy_gh_farmer/error_reporting.py
+++ foxy_gh_farmer/error_reporting.py
@@ -1,4 +1,5 @@
 import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
 
 from foxy_gh_farmer.version import version
 
@@ -7,4 +8,7 @@ def init_sentry():
     sentry_sdk.init(
         dsn="https://dd8679dd489b6fe597c1c30bfdecc5b4@o236153.ingest.sentry.io/4506149108252672",
         release=f"foxy-gh-farmer@{version}",
+        integrations=[
+            LoggingIntegration(event_level=None),
+        ],
     )
